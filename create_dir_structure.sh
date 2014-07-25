a3d735af3e1ad3c7964cc9d3e5c36696c778bf47
Aiden <silversunseven@gmail.com>
2014-07-25 07:20:15 +0200

    Commiting changes

---
+++ create_dir_structure.sh
@@ -0,0 +1,156 @@
+mkdir -p Library
+cd Library
+mkdir -p ./300\ Rise\ Of\ An\ Empire\ 2014\ HC\ 720p\ WEBRip\ x264\ AC3-JYK/
+touch ./300\ Rise\ Of\ An\ Empire\ 2014\ HC\ 720p\ WEBRip\ x264\ AC3-JYK/300\ Rise\ Of\ An\ Empire\ 2014\ HC\ 720p\ WEBRip\ x264\ AC3-JYK.mkv
+mkdir -p ./Age\ of\ Tomorrow\ \(2014\)\ [1080p]
+touch ./Age\ of\ Tomorrow\ \(2014\)\ [1080p]/Age.of.Tomorrow.2014.1080p.BluRay.x264.YIFY.mp4
+mkdir -p ./All\ Is\ Lost\ \(2013\)
+touch ./All\ Is\ Lost\ \(2013\)/All.Is.Lost.2013.720p.BluRay.x264.YIFY.mp4
+touch ./All\ Is\ Lost\ \(2013\)/WWW.YIFY-TORRENTS.COM.jpg
+mkdir -p ./Appleseed\ Alpha\ 2014\ 1080p\ BRRip\ x264\ DTS-JYK
+touch ./Appleseed\ Alpha\ 2014\ 1080p\ BRRip\ x264\ DTS-JYK/Appleseed\ Alpha\ 2014\ 1080p\ BRRip\ x264\ DTS-JYK.mkv
+touch ./Appleseed\ Alpha\ 2014\ 1080p\ BRRip\ x264\ DTS-JYK/Appleseed\ Alpha\ 2014\ 1080p\ BRRip\ x264\ DTS-JYK.mkv.jpg
+mkdir -p ./Captain.America.The.First.Avenger.2011.1080p.BRRip.x264.AAC-m2g
+touch ./Captain.America.The.First.Avenger.2011.1080p.BRRip.x264.AAC-m2g/Captain.America.The.First.Avenger.2011.1080p.BRRip.x264.AAC-m2g.\(Sample\).mkv
+touch ./Captain.America.The.Winter.Soldier.2014.720p.HDTV.XviD.AC3-VAiN/Captain.America.The.Winter.Soldier.2014.720p.HDTV.XviD.AC3-VAiN.avi
+mkdir -p ./Captain.America.The.Winter.Soldier.2014.720p.HDTV.XviD.AC3-VAiN
+touch ./Captain.America.The.Winter.Soldier.2014.720p.HDTV.XviD.AC3-VAiN/sample.avi
+mkdir -p ./Flipped\ \(2010\)\ [1080p]
+touch ./Flipped\ \(2010\)\ [1080p]/Flipped.2010.1080p.BluRay.x264.YIFY.mp4
+touch ./Flipped\ \(2010\)\ [1080p]/WWW.YIFY-TORRENTS.COM.jpg
+mkdir -p ./Free\ Birds\ \(2013\)\ [1080p]
+touch ./Free\ Birds\ \(2013\)\ [1080p]/Free.Birds.2013.1080p.BluRay.x264.YIFY.mp4
+touch ./Free\ Birds\ \(2013\)\ [1080p]/WWW.YIFY-TORRENTS.COM.jpg
+mkdir -p ./Grudge\ Match\ \(2013\)\ [1080p]
+touch ./Grudge\ Match\ \(2013\)\ [1080p]/Grudge.Match.2013.1080p.BluRay.x264.YIFY.mp4
+touch ./Grudge\ Match\ \(2013\)\ [1080p]/WWW.YIFY-TORRENTS.COM.jpg
+mkdir -p ./Heaven\ Is\ for\ Real\ \(2014\)
+touch ./Heaven\ Is\ for\ Real\ \(2014\)/Heaven.Is.for.Real.2014.720p.BluRay.x264.YIFY.mp4
+touch ./Heaven\ Is\ for\ Real\ \(2014\)/WWW.YIFY-TORRENTS.COM.jpg
+mkdir -p ./Ill\ Follow\ You\ Down\ \(2013\)
+touch ./Ill\ Follow\ You\ Down\ \(2013\)/Ill.Follow.You.Down.2013.720p.BluRay.x264.YIFY.mp4
+touch ./Ill\ Follow\ You\ Down\ \(2013\)/WWW.YTS.RE.jpg
+mkdir -p ./IPT/Ephraims.Rescue.2013.BDRip.XviD.Lum1x
+touch ./IPT/Ephraims.Rescue.2013.BDRip.XviD.Lum1x/Ephraims.Rescue.2013.BDRip.XviD.Lum1x.txt
+touch ./IPT/Ephraims.Rescue.2013.BDRip.XviD.Lum1x/Ephraims.Rescue.2013.BDRip.XviD.Lum1x.zip
+mkdir -p ./IPT/Fast.And.Furious.6.2013.EXTENDED.1080p.BluRay.DTS.x264-PHD
+touch ./IPT/Fast.And.Furious.6.2013.EXTENDED.1080p.BluRay.DTS.x264-PHD/Fast.And.Furious.6.2013.EXTENDED.1080p.BluRay.DTS.x264-PHD.mkv
+mkdir -p ./IPT/Gravity.2013.720p.DVDSCR.XViD.AC3-LEGi0N
+touch ./IPT/Gravity.2013.720p.DVDSCR.XViD.AC3-LEGi0N/Gravity.2013.720p.DVDSCR.XViD.AC3-LEGi0N.avi
+touch ./IPT/Gravity.2013.720p.DVDSCR.XViD.AC3-LEGi0N/Gravity.2013.720p.DVDSCR.XViD.AC3-LEGi0N.nfo
+touch ./IPT/Gravity.2013.720p.DVDSCR.XViD.AC3-LEGi0N/Gravity.2013.720p.DVDSCR.XViD.AC3-LEGi0N.sample.avi
+mkdir -p ./IPT/The.Lion.King.1994.HDTV.720p.x264.aZA
+touch ./IPT/The.Lion.King.1994.HDTV.720p.x264.aZA/The.Lion.King.1994.HDTV.720p.x264.aZA.mkv
+touch ./IPT/The.Lion.King.1994.HDTV.720p.x264.aZA/The.Lion.King.1994.HDTV.720p.x264.aZA.mkv_snapshot_00.02.png
+touch ./IPT/The.Lion.King.1994.HDTV.720p.x264.aZA/The.Lion.King.1994.HDTV.720p.x264.aZA.mkv_snapshot_00.43.06_[2010.06.24_21.12.28].png
+touch ./IPT/The.Lion.King.1994.HDTV.720p.x264.aZA/The.Lion.King.1994.HDTV.720p.x264.aZA.mkv_snapshot_01.09.41_[2010.06.24_21.12.56].png
+touch ./IPT/The.Lion.King.1994.HDTV.720p.x264.aZA/The.Lion.King.1994.HDTV.720p.x264.aZA.mkv_thumbs_[2010.06.24_21.13.11].png
+touch ./IPT/The.Lion.King.1994.HDTV.720p.x264.aZA/The.Lion.King.1994.HDTV.720p.x264.aZA.png
+touch ./IPT/The.Lion.King.1994.HDTV.720p.x264.aZA/The.Lion.King.1994.HDTV.720p.x264.aZA.sample.mkv
+mkdir -p ./IPT/The.Mortal.Instruments.City.of.Bones.2013.BRRip.XviD-AQOS
+touch ./IPT/The.Mortal.Instruments.City.of.Bones.2013.BRRip.XviD-AQOS/aqos-tmicob.2013.brrip.xvid.avi
+touch ./IPT/The.Mortal.Instruments.City.of.Bones.2013.BRRip.XviD-AQOS/aqos-tmicob.2013.brrip.xvid.nfo
+touch ./IPT/The.Mortal.Instruments.City.of.Bones.2013.BRRip.XviD-AQOS/sample.avi
+mkdir -p ./Jamesy\ Boy\ \(2014\)
+touch ./Jamesy\ Boy\ \(2014\)/Jamesy.Boy.2014.720p.BluRay.x264.YIFY.mp4
+touch ./Jamesy\ Boy\ \(2014\)/WWW.YIFY-TORRENTS.COM.jpg
+mkdir -p ./Khumba.2013.BDRiP.AC3-5.1.XviD-AXED
+touch ./Khumba.2013.BDRiP.AC3-5.1.XviD-AXED/Khumba.2013.BDRiP.AC3-5.1.XviD-AXED.avi
+touch ./Khumba.2013.BDRiP.AC3-5.1.XviD-AXED/Khumba.2013.BDRiP.AC3-5.1.XviD-AXED.nfo
+mkdir -p ./Mr\ Peabody\ and\ Sherman\ 2014\ 720p\ WEB-DL\ x264\ AAC-JYK/subs
+touch ./Mr\ Peabody\ and\ Sherman\ 2014\ 720p\ WEB-DL\ x264\ AAC-JYK/Mr\ Peabody\ and\ Sherman\ 2014\ 720p\ WEB-DL\ x264\ AAC-JYK.mp4
+touch ./Mr\ Peabody\ and\ Sherman\ 2014\ 720p\ WEB-DL\ x264\ AAC-JYK/Mr\ Peabody\ and\ Sherman\ 2014\ 720p\ WEB-DL\ x264\ AAC-JYK.mp4.jpg
+touch ./Mr\ Peabody\ and\ Sherman\ 2014\ 720p\ WEB-DL\ x264\ AAC-JYK/subs/\(eng\)Mr\ Peabody\ and\ Sherman\ 2014\ 720p\ WEB-DL\ x264\ AAC-JYK.srt
+touch ./Mr\ Peabody\ and\ Sherman\ 2014\ 720p\ WEB-DL\ x264\ AAC-JYK/subs/Mr\ Peabody\ and\ Sherman\ 2014\ 720p\ WEB-DL\ x264\ AAC-JYK.smi
+mkdir -p ./Need\ for\ Speed\ \(2014\)\ [1080p]
+touch ./Need\ for\ Speed\ \(2014\)\ [1080p]/Need.for.Speed.2014.1080p.BluRay.x264.YIFY.mp4
+touch ./Need\ for\ Speed\ \(2014\)\ [1080p]/WWW.YTS.RE.jpg
+mkdir -p ./Noah.2013.720p.BluRay.x264-SPARKS[rarbg]/Subs
+touch ./Noah.2013.720p.BluRay.x264-SPARKS[rarbg]/Noah.2013.720p.BluRay.x264-SPARKS.mkv
+touch ./Noah.2013.720p.BluRay.x264-SPARKS[rarbg]/noah.2013.720p.bluray.x264-sparks.nfo
+touch ./Noah.2013.720p.BluRay.x264-SPARKS[rarbg]/RARBG.com.txt
+touch ./Noah.2013.720p.BluRay.x264-SPARKS[rarbg]/Subs/Noah.2013.720p.BluRay.x264-SPARKS.idx
+touch ./Noah.2013.720p.BluRay.x264-SPARKS[rarbg]/Subs/Noah.2013.720p.BluRay.x264-SPARKS.sub
+mkdir -p ./Otto.The.Rhino.2013.WEBRIP.x264.AC3.CrEwSaDe
+touch ./Otto.The.Rhino.2013.WEBRIP.x264.AC3.CrEwSaDe/CrEwSaDe.nfo
+touch ./Otto.The.Rhino.2013.WEBRIP.x264.AC3.CrEwSaDe/Otto.The.Rhino.2013.WEBRIP.x264.AC3.CrEwSaDe.mkv
+touch ./Otto.The.Rhino.2013.WEBRIP.x264.AC3.CrEwSaDe/Otto.The.Rhino.2013.WEBRIP.x264.AC3.CrEwSaDe.Sample.mkv
+mkdir -p ./Philomena\ \(2013\)
+touch ./Philomena\ \(2013\)/Philomena.2013.720p.BluRay.x264.YIFY.mp4
+touch ./Philomena\ \(2013\)/WWW.YIFY-TORRENTS.COM.jpg
+mkdir -p ./Planes.2013.German.DVDRiP.AC3.XviD-CRG
+touch ./Planes.2013.German.DVDRiP.AC3.XviD-CRG/Planes.2013.German.DVDRiP.AC3.XviD-CRG.avi
+touch ./Planes.2013.German.DVDRiP.AC3.XviD-CRG/Planes.2013.German.DVDRiP.AC3.XviD-CRG.nfo
+mkdir -p ./Rio\ 2\ \(2014\)
+touch ./Rio\ 2\ \(2014\)/Rio.2.2014.720p.BluRay.x264.YIFY.mp4
+touch ./Rio\ 2\ \(2014\)/WWW.YIFY-TORRENTS.COM.jpg
+mkdir -p ./Saving\ Mr\ Banks\ 2013\ DVDSCR[AC3]\ juggs
+touch ./Saving\ Mr\ Banks\ 2013\ DVDSCR[AC3]\ juggs/sample.avi
+touch ./Saving\ Mr\ Banks\ 2013\ DVDSCR[AC3]\ juggs/Saving\ Mr\ Banks\ 2013\ DVDSCR[AC3]\ juggs.avi
+touch ./Saving\ Mr\ Banks\ 2013\ DVDSCR[AC3]\ juggs/Torrent\ Downloaded\ From\ ExtraTorrent.cc.txt
+mkdir -p ./Son\ of\ Batman\ [2014]\ BRRip\ XviD-SaM[ETRG]/
+touch ./Son\ of\ Batman\ [2014]\ BRRip\ XviD-SaM[ETRG]/SaMple.avi
+touch ./Son\ of\ Batman\ [2014]\ BRRip\ XviD-SaM[ETRG]/Son\ of\ Batman\ [2014]\ BRRip\ XviD-SaM[ETRG].avi
+touch ./Son\ of\ Batman\ [2014]\ BRRip\ XviD-SaM[ETRG]/Son\ of\ Batman\ [2014]\ BRRip\ XviD-SaM[ETRG].srt
+touch ./Son\ of\ Batman\ [2014]\ BRRip\ XviD-SaM[ETRG]/Son\ of\ Batman\ [2014]\ BRRip\ XviD-SaM[ETRG]HI.srt
+touch ./Son\ of\ Batman\ [2014]\ BRRip\ XviD-SaM[ETRG]/SONOFBATMAN-ETRG.nfo
+touch ./Son\ of\ Batman\ [2014]\ BRRip\ XviD-SaM[ETRG]/Torrent\ Downloaded\ From\ ExtraTorrent.cc.txt
+mkdir -p ./Son\ of\ God\ \(2014\)
+touch ./Son\ of\ God\ \(2014\)/Son.of.God.2014.720p.BluRay.x264.YIFY.mp4
+touch ./Son\ of\ God\ \(2014\)/WWW.YIFY-TORRENTS.COM.jpg
+mkdir -p ./Soul\ Surfer\ \(2011\)\ [1080p]
+touch ./Soul\ Surfer\ \(2011\)\ [1080p]/Soul.Surfer.2011.1080p.BluRay.x264.YIFY.mp4
+touch ./Soul\ Surfer\ \(2011\)\ [1080p]/WWW.YIFY-TORRENTS.COM.jpg
+mkdir -p ./The\ Adventurer\ The\ Curse\ of\ the\ Midas\ Box\ [2013]\ 1080p\ BluRay\ AAC\ x264-tomcat12[ETRG]
+touch ./The\ Adventurer\ The\ Curse\ of\ the\ Midas\ Box\ [2013]\ 1080p\ BluRay\ AAC\ x264-tomcat12[ETRG]/The\ Curse\ of\ the\ Midas\ Box\ [2013]\ 1080p\ BluRay\ AAC\ x264-tomcat12[ETRG].mp4
+touch ./The\ Adventurer\ The\ Curse\ of\ the\ Midas\ Box\ [2013]\ 1080p\ BluRay\ AAC\ x264-tomcat12[ETRG]/The\ Curse\ of\ the\ Midas\ Box\ [2013]\ 1080p\ sample.mp4
+touch ./The\ Adventurer\ The\ Curse\ of\ the\ Midas\ Box\ [2013]\ 1080p\ BluRay\ AAC\ x264-tomcat12[ETRG]/Torrent\ Downloaded\ From\ ExtraTorrent.cc.txt
+mkdir -p ./The\ Legend\ of\ Hercules\ \(2014\)
+touch ./The\ Legend\ of\ Hercules\ \(2014\)/The.Legend.of.Hercules.2014.720p.BluRay.x264.YIFY.mp4
+touch ./The\ Legend\ of\ Hercules\ \(2014\)/WWW.YIFY-TORRENTS.COM.jpg
+mkdir -p ./The\ Lego\ Movie\ \(2014\)
+touch ./The\ Lego\ Movie\ \(2014\)/The.Lego.Movie.2014.720p.BluRay.x264.YIFY.mp4
+touch ./The\ Lego\ Movie\ \(2014\)/WWW.YIFY-TORRENTS.COM.jpg
+mkdir -p ./The\ Nut\ Job\ \(2014\)
+touch ./The\ Nut\ Job\ \(2014\)/The.Nut.Job.2014.720p.BluRay.x264.YIFY.mp4
+touch ./The\ Nut\ Job\ \(2014\)/WWW.YIFY-TORRENTS.COM.jpg
+mkdir -p ./The\ Other\ Woman\ \(2014\)
+touch ./The\ Other\ Woman\ \(2014\)/The.Other.Woman.2014.720p.BluRay.x264.YIFY.mp4
+touch ./The\ Other\ Woman\ \(2014\)/WWW.YTS.RE.jpg
+mkdir -p ./The\ Pirate\ Fairy\ \(2014\)
+touch ./The\ Pirate\ Fairy\ \(2014\)/The.Pirate.Fairy.2014.720p.BluRay.x264.YIFY.mp4
+touch ./The\ Pirate\ Fairy\ \(2014\)/WWW.YIFY-TORRENTS.COM.jpg
+mkdir -p ./Tracks\ \(2013\)
+touch ./Tracks\ \(2013\)/Tracks.2013.720p.BluRay.x264.YIFY.mp4
+touch ./Tracks\ \(2013\)/WWW.YIFY-TORRENTS.COM.jpg
+mkdir -p ./Video.Games.The.Movie.2014.HDRip.XViD-juggs[ETRG]
+touch ./Video.Games.The.Movie.2014.HDRip.XViD-juggs[ETRG]/ETRG.mp4
+touch ./Video.Games.The.Movie.2014.HDRip.XViD-juggs[ETRG]/sample.avi
+touch ./Video.Games.The.Movie.2014.HDRip.XViD-juggs[ETRG]/Torrent\ Downloaded\ From\ ExtraTorrent.cc.txt
+touch ./Video.Games.The.Movie.2014.HDRip.XViD-juggs[ETRG]/Video\ Games\ The\ Movie-ETRG.nfo
+touch ./Video.Games.The.Movie.2014.HDRip.XViD-juggs[ETRG]/Video.Games.The.Movie.2014.HDRip.XViD-juggs[ETRG].avi
+touch ./Video.Games.The.Movie.2014.HDRip.XViD-juggs[ETRG]/Video.Games.The.Movie.2014.HDRip.XViD-juggs[ETRG].srt
+mkdir -p ./Walking.With.Dinosaurs.2013.720p.BRRiP.XViD.AC3-LEGi0N
+touch ./Walking.With.Dinosaurs.2013.720p.BRRiP.XViD.AC3-LEGi0N/T3_Subtitle\ -\ English.srt
+touch ./Walking.With.Dinosaurs.2013.720p.BRRiP.XViD.AC3-LEGi0N/Walking.With.Dinosaurs.2013.720p.BRRiP.XViD.AC3-LEGi0N.avi
+touch ./Walking.With.Dinosaurs.2013.720p.BRRiP.XViD.AC3-LEGi0N/Walking.With.Dinosaurs.2013.720p.BRRiP.XViD.AC3-LEGi0N.nfo
+touch ./Walking.With.Dinosaurs.2013.720p.BRRiP.XViD.AC3-LEGi0N/Walking.With.Dinosaurs.2013.720p.BRRiP.XViD.AC3-LEGi0N.sample.avi
+mkdir -p ./Wings.Sky\ Force.Heroes.2014.HDRip.XViD.juggs[ETRG]
+touch ./Wings.Sky\ Force.Heroes.2014.HDRip.XViD.juggs[ETRG]/ETRG.mp4
+touch ./Wings.Sky\ Force.Heroes.2014.HDRip.XViD.juggs[ETRG]/sample.avi
+touch ./Wings.Sky\ Force.Heroes.2014.HDRip.XViD.juggs[ETRG]/Torrent\ Downloaded\ From\ ExtraTorrent.cc.txt
+touch ./Wings.Sky\ Force.Heroes.2014.HDRip.XViD.juggs[ETRG]/Wings\ Sky\ Force\ Heroes-ETRG.nfo
+touch ./Wings.Sky\ Force.Heroes.2014.HDRip.XViD.juggs[ETRG]/Wings.Sky\ Force.Heroes.2014.HDRip.XViD.juggs[ETRG].avi
+touch ./Wings.Sky\ Force.Heroes.2014.HDRip.XViD.juggs[ETRG]/Wings.Sky\ Force.Heroes.2014.HDRip.XViD.juggs[ETRG].srt
+mkdir -p ./X\ Men\ First\ Class\ \(2011\)/Other
+touch ./X\ Men\ First\ Class\ \(2011\)/Foreign\ .Subs.Only.YIFY.srt
+touch ./X\ Men\ First\ Class\ \(2011\)/Other/AhaShare.com.txt
+touch ./X\ Men\ First\ Class\ \(2011\)/Other/Torrent\ downloaded\ from\ Demonoid.com\ -\ Copy.txt
+touch ./X\ Men\ First\ Class\ \(2011\)/WWW.YIFY-TORRENTS.COM.jpg
+touch ./X\ Men\ First\ Class\ \(2011\)/X-Men.First.Class.2011.720p.BrRip.264.YIFY.mp4
+touch ./X\ Men\ First\ Class\ \(2011\)/X-Men.First.Class.2011.720pBrRip.264.YIFY.srt
+mkdir -p ./Zarafa\ \(2012:2014\)\ DVDrip\ \(xvid\)\ NL\ Gespr.\ DMT_
+touch ./Zarafa\ \(2012:2014\)\ DVDrip\ \(xvid\)\ NL\ Gespr.\ DMT_/DMT\ NFO,\ Read\ before\ Playing.txt
+touch ./Zarafa\ \(2012:2014\)\ DVDrip\ \(xvid\)\ NL\ Gespr.\ DMT_/dmt4.png
+touch ./Zarafa\ \(2012:2014\)\ DVDrip\ \(xvid\)\ NL\ Gespr.\ DMT_/jojo.png
+touch ./Zarafa\ \(2012:2014\)\ DVDrip\ \(xvid\)\ NL\ Gespr.\ DMT_/zarafa.avi
