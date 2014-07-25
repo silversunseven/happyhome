4f5868a4f07fa544d045f8da5414e598e1cc80a2
Aiden <silversunseven@gmail.com>
2014-07-25 09:37:08 +0200

    updating script with a larger library

--- create_dir_structure.sh
+++ create_dir_structure.sh
@@ -37,14 +37,6 @@ mkdir -p ./IPT/Gravity.2013.720p.DVDSCR.XViD.AC3-LEGi0N
 touch ./IPT/Gravity.2013.720p.DVDSCR.XViD.AC3-LEGi0N/Gravity.2013.720p.DVDSCR.XViD.AC3-LEGi0N.avi
 touch ./IPT/Gravity.2013.720p.DVDSCR.XViD.AC3-LEGi0N/Gravity.2013.720p.DVDSCR.XViD.AC3-LEGi0N.nfo
 touch ./IPT/Gravity.2013.720p.DVDSCR.XViD.AC3-LEGi0N/Gravity.2013.720p.DVDSCR.XViD.AC3-LEGi0N.sample.avi
-mkdir -p ./IPT/The.Lion.King.1994.HDTV.720p.x264.aZA
-touch ./IPT/The.Lion.King.1994.HDTV.720p.x264.aZA/The.Lion.King.1994.HDTV.720p.x264.aZA.mkv
-touch ./IPT/The.Lion.King.1994.HDTV.720p.x264.aZA/The.Lion.King.1994.HDTV.720p.x264.aZA.mkv_snapshot_00.02.png
-touch ./IPT/The.Lion.King.1994.HDTV.720p.x264.aZA/The.Lion.King.1994.HDTV.720p.x264.aZA.mkv_snapshot_00.43.06_[2010.06.24_21.12.28].png
-touch ./IPT/The.Lion.King.1994.HDTV.720p.x264.aZA/The.Lion.King.1994.HDTV.720p.x264.aZA.mkv_snapshot_01.09.41_[2010.06.24_21.12.56].png
-touch ./IPT/The.Lion.King.1994.HDTV.720p.x264.aZA/The.Lion.King.1994.HDTV.720p.x264.aZA.mkv_thumbs_[2010.06.24_21.13.11].png
-touch ./IPT/The.Lion.King.1994.HDTV.720p.x264.aZA/The.Lion.King.1994.HDTV.720p.x264.aZA.png
-touch ./IPT/The.Lion.King.1994.HDTV.720p.x264.aZA/The.Lion.King.1994.HDTV.720p.x264.aZA.sample.mkv
 mkdir -p ./IPT/The.Mortal.Instruments.City.of.Bones.2013.BRRip.XviD-AQOS
 touch ./IPT/The.Mortal.Instruments.City.of.Bones.2013.BRRip.XviD-AQOS/aqos-tmicob.2013.brrip.xvid.avi
 touch ./IPT/The.Mortal.Instruments.City.of.Bones.2013.BRRip.XviD-AQOS/aqos-tmicob.2013.brrip.xvid.nfo
@@ -63,12 +55,12 @@ touch ./Mr\ Peabody\ and\ Sherman\ 2014\ 720p\ WEB-DL\ x264\ AAC-JYK/subs/Mr\ Pe
 mkdir -p ./Need\ for\ Speed\ \(2014\)\ [1080p]
 touch ./Need\ for\ Speed\ \(2014\)\ [1080p]/Need.for.Speed.2014.1080p.BluRay.x264.YIFY.mp4
 touch ./Need\ for\ Speed\ \(2014\)\ [1080p]/WWW.YTS.RE.jpg
-mkdir -p ./Noah.2013.720p.BluRay.x264-SPARKS[rarbg]/Subs
-touch ./Noah.2013.720p.BluRay.x264-SPARKS[rarbg]/Noah.2013.720p.BluRay.x264-SPARKS.mkv
-touch ./Noah.2013.720p.BluRay.x264-SPARKS[rarbg]/noah.2013.720p.bluray.x264-sparks.nfo
-touch ./Noah.2013.720p.BluRay.x264-SPARKS[rarbg]/RARBG.com.txt
-touch ./Noah.2013.720p.BluRay.x264-SPARKS[rarbg]/Subs/Noah.2013.720p.BluRay.x264-SPARKS.idx
-touch ./Noah.2013.720p.BluRay.x264-SPARKS[rarbg]/Subs/Noah.2013.720p.BluRay.x264-SPARKS.sub
+mkdir -p ./Noah.2014.720p.BluRay.x264-SPARKS[rarbg]/Subs
+touch ./Noah.2014.720p.BluRay.x264-SPARKS[rarbg]/Noah.2014.720p.BluRay.x264-SPARKS.mkv
+touch ./Noah.2014.720p.BluRay.x264-SPARKS[rarbg]/noah.2014.720p.bluray.x264-sparks.nfo
+touch ./Noah.2014.720p.BluRay.x264-SPARKS[rarbg]/RARBG.com.txt
+touch ./Noah.2014.720p.BluRay.x264-SPARKS[rarbg]/Subs/Noah.2014.720p.BluRay.x264-SPARKS.idx
+touch ./Noah.2014.720p.BluRay.x264-SPARKS[rarbg]/Subs/Noah.2014.720p.BluRay.x264-SPARKS.sub
 mkdir -p ./Philomena\ \(2013\)
 touch ./Philomena\ \(2013\)/Philomena.2013.720p.BluRay.x264.YIFY.mp4
 touch ./Philomena\ \(2013\)/WWW.YIFY-TORRENTS.COM.jpg
@@ -148,3 +140,125 @@ touch ./Zarafa\ \(2012:2014\)\ DVDrip\ \(xvid\)\ NL\ Gespr.\ DMT_/DMT\ NFO,\ Rea
 touch ./Zarafa\ \(2012:2014\)\ DVDrip\ \(xvid\)\ NL\ Gespr.\ DMT_/dmt4.png
 touch ./Zarafa\ \(2012:2014\)\ DVDrip\ \(xvid\)\ NL\ Gespr.\ DMT_/jojo.png
 touch ./Zarafa\ \(2012:2014\)\ DVDrip\ \(xvid\)\ NL\ Gespr.\ DMT_/zarafa.avi
+touch ./The\ River\ Within.avi
+touch ./Beyond\ the\ Night\ 1983.avi
+touch ./Me\ You\ Us\ Forever.avi
+touch ./Oblivion.2013.1080p.BluRay.x264.YIFY.mp4
+touch ./In\ the\ blink\ of\ an\ eye[DVDrip].avi
+touch ./The\ Moment\ After\ 2\ -\ The\ Awakening\(2006\(.avi
+touch ./Diary.of.a.Wimpy.Kid.Dog.Days.2012.720p.BrRip.x264.YIFY.mp4
+touch ./The\ Sobbing\ Stone\ 2005\ CG\ entheta.avi
+touch ./The\ Apostle.avi
+touch ./Pilgrims\ Progress\ -\ Journey\ To\ Heaven\ \(2008\(.avi
+touch ./The\ Perfect\ Stranger.avi
+touch ./MONEY\ OR\ MISSION.divx
+touch ./Die.Welle.2008.720p.BluRay.x264-CiNEFiLE.mkv
+touch ./The\ Moment\ After.avi
+touch ./Final\ Exit.avi
+touch ./Logan.mp4
+touch ./Leviathan.2012.1080p.BluRay.x264.YIFY.mp4
+touch ./LEGO.Star.Wars.The.Yoda.Chronicles.2013.DVDRip.XviD.AC3-EVO.avi
+touch ./Playing.For.Keeps.2012.720p.BluRay.x264.YIFY.mp4
+touch ./Here.Comes.the.Boom.BDRip.XviD.CD2-COCAIN.avi
+touch ./list
+touch ./In\ Time\ of\ Need.divx
+touch ./The\ Second\ Chance[2006].avi
+touch ./Lion\ King\ 1\ 1994.mp4
+touch ./Mandela.Long.Walk.to.Freedom.2013.DVDSCR.XViD.AC3-LEGi0N.avi
+touch ./Flywheel\ \(2003\(.avi
+touch ./ANOTHER\ PERFECT\ STRANGER.avi
+touch ./Quantum.of.Solace.2008.720p.AAC.x264.mkv
+touch ./The\ Inn\ Of\ The\ Sixth\ Happiness.avi
+touch ./I\ Now\ Pronounce\ You\ Chuck\ and\ Larry[2007]BRRip\ XviD-ETRG.avi
+touch ./The\ Encounter.avi
+touch ./Pamelas\ Prayer.avi
+touch ./A\ Walk\ to\ Remember\ \(2002\(\ DvDrip\ Xvid..avi
+touch ./The\ Field\ is\ White.mov
+touch ./The\ Imposter[2010][DVDRip].avi
+touch ./Dolphin.Tale.2011.720p.BrRip.x264.YIFY.mp4
+touch ./Escape.from.Planet.Earth.2013.720p.BluRay.x264.YIFY.mp4
+touch ./Admission.2013.720p.BluRay.x264.YIFY.mp4
+touch ./The\ Appointment.avi
+touch ./Aliens.of.the.Deep.2005.720p.WEB-DL.HDCLUB.mkv
+touch ./Book\ of\ Mormon\ Journey\ Vol\ 1.mov
+touch ./ETRG.mp4
+touch ./After\ Earth\ 2013\ 1080p\ WEB-DLRip\ x264\ AAC-JYK.mkv
+touch ./Father\ in\ Israel.2009.mkv
+touch ./The\ Back-Up\ Plan\ \(2010\(\ 720p.mkv
+touch ./Extras.avi
+touch ./American\ Mormon.divx
+touch ./T.L.W.BRR.ANALOG.avi
+touch ./Mandie.and.the.Secret.Tunnel.2009.DVDRip.XviD-ViSiON.avi
+touch ./The\ Grace\ Card.avi
+touch ./Iron.Man.3.2013.1080p.BluRay.x264.YIFY.mp4
+touch ./Extraordinary\ Measures\ 2010\ 720p\ BRRip\ H264\ AAC-GiPSY.mp4
+touch ./Letters.To.God[2010]DvDrip-aXXo.avi
+touch ./Hancock\ County.mov
+touch ./The\ Climb\ -\ the\ making\ of.avi
+touch ./A.Case.of.You.2013.HDRip.XviD.AC3-EVO.avi
+touch ./AMERICAN\ MORMON\ in\ EUROPE.divx
+touch ./Lehis\ Land\ of\ First\ Inheritance-Exploring\ the\ Lands\ of\ the\ Book\ of\ Mormon\ .divx
+touch ./Man.of.Tai.Chi.2013.720p.Webrip.x264.AC3-FooKaS.mkv
+touch ./Here.Comes.the.Boom.BDRip.XviD.CD1-COCAIN.avi
+touch ./Black\ Nativity\ 2013\ DVDRip\ XviD-FAVIK.avi
+touch ./ExtraTorrentRG.mp4
+touch ./Epic.2013.720p.BluRay.x264.YIFY.mp4
+touch ./Bourne\ the\ 2\ 2004\ Supremacy.mp4
+touch ./Luther-AC3-5,1-DVDRip[Eng]2003.avi
+touch ./Clancy.avi
+touch ./CHRISTIAN\ MOVIE--BEHIND\ THE\ SUN.avi
+touch ./Haunter.2013.720p.BluRay.x264.YIFY.mp4
+touch ./Jerusalem.Countdown.avi
+touch ./The\ Grandmasters\ 2013\ 720p\ WEBRip\ x264\ AC3-JYK.mkv
+touch ./Courageous.2011.BluRay.720p.x264.DTS-HDChina.mkv
+touch ./Amazing\ Racer\ 2013\ BRRiP\ XViD-sC0rp.avi
+touch ./Diary.of.a.Wimpy.Kid.2.Rodrick.Rules.2011.BRRip.AC3.XViD-EP1C.avi
+touch ./Hotel.Transylvania.2012.720p.BluRay.x264.YIFY.mp4
+touch ./Most\ -\ The\ Bridge\ \(2003\(.avi
+touch ./Pitch.Perfect.2012.720p.BRrip.264.YIFY.mp4
+touch ./Journey\ to\ Faith.divx
+touch ./Belle\ And\ The\ Beast.avi
+touch ./Brigham\ City.mov
+touch ./Love\ Comes\ Softly\ \(Christian\ Movie\(.avi
+touch ./The\ Secret\ Life\ of\ Walter\ Mitty\ 2013\ DVDscr\ XViD\ NO1KNOWS.avi
+touch ./Max\ Lucado\ Story\ -\ Christmas\ Child\ \(2003\(.avi
+touch ./Bourne\ the\ 3\ 2007\ Ultimatum.mp4
+touch ./Dark.Skies.2013.720p.Read.Info.X264.AC3.TommieCook.avi
+touch ./Journey\ 2\ The\ Mysterious\ Island\ 2012\ TS\ READNFO\ XViD\ -\ INSPiRAL.avi
+touch ./Christmas\ In\ Canaan.avi
+touch ./Amazing\ Love.avi
+touch ./Dialtone\ [2009][DVDRip].avi
+touch ./Mud.2012.720p.BluRay.x264.YIFY.mp4
+touch ./The\ Book\ Thief\ [2013]\ DVDScr\ XviD-SaM[ETRG].avi
+touch ./The\ Messiah\ -\ Prophecy\ Fulfilled.avi
+touch ./Escape\ From\ Hell.avi
+touch ./The\ Good\ Samaritan.divx
+touch ./Mama.2013.720p.BRrip.x264.GAZ.mp4
+touch ./Down\ and\ Derby.mov
+touch ./Lay.It.Down.avi
+touch ./Color\ Of\ The\ Cross\ 2.avi
+touch ./The\ Knot\ \(2012\(\ DVDRip\ XViD\ juggs.avi
+touch ./Gods.country.avi
+touch ./Tears\ of\ Steel\ \(2012\(\ BRRIP\ DD\ 2.0\ SRT\ XVID\ EXTERNE\ SUBS.avi
+touch ./Breakout.2013.DVDRip.XviD-3LT0N.avi
+touch ./Luther-AC3-5,1-DVDRip[Eng]2003.txt
+touch ./Joseph.mov
+touch ./The\ Perfect\ Game.avi
+touch ./Out\ of\ Step.divx
+touch ./Lemonade\ Mouth[2011]DVDRip\ XviD-ExtraTorrentRG.avi
+touch ./House.At.The.End.Of.The.Street.2012.720p.BluRay.x264.YIFY.mp4
+touch ./The\ Encounter\ 2.avi
+touch ./The\ Climb\ \(2002\(\ English\ subs.avi
+touch ./The\ Host\ 2013\ 720p\ HDRip\ x264\ AC3-JYK.mkv
+touch ./Color\ of\ the\ Cross.English_French.sub.DVDriP.avi
+touch ./Hours.2013.720p.BluRay.x264.YIFY.mp4
+touch ./Anonymous\ 2011\ 720p\ BRRip\ x264\ -MgB.mp4
+touch ./Bourne\ the\ 1\ 2002\ Identity.mp4
+touch ./God\ of\ Wonders.avi
+touch ./The\ Legend\ of\ Three\ Trees.avi
+touch ./One\ Day[2011]BRRip\ XviD-ExtraTorrentRG.avi
+touch ./The\ Counterfeiters\ \(aka\ Die\ Falscher\(\ KLAXXON.avi
+touch ./Late\ One\ Night.avi
+touch ./End_of_the_harvest.mpg
+touch ./Age.of.Dinosaurs.2013.1080p.BluRay.x264.YIFY.mp4
+touch ./The\ Iceman\ 2012\ 1080p.mkv
